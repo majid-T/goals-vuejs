fae57947340f0a970646d2a904dbab0c291305a0
Majid Shockoohi <majid.shockoohi@gmail.com>
2020-09-11 19:21:39 -0400

    Added loading effect to input and btn

--- client/src/components/AddGoal.vue
+++ client/src/components/AddGoal.vue
@@ -1,9 +1,13 @@
 <template>
 <div class="create-goal">
-    <input type="text" id="create-goal" class="input-goal" v-model="newGoal" placeholder="My new Goal" />
-    <button @click="addGoal"><i class="fas fa-plus"></i></button>
+    <input type="text" id="create-goal" class="input-goal" v-bind:disabled="loading" v-model="newGoal" placeholder="My new Goal" />
+    <button @click="addGoal" v-bind:disabled="newGoal.length === 0 || loading">
+        <i v-bind:class="[loading ? 'fas fa-sync-alt fa-spin' : 'fas fa-plus']"></i>
+    </button>
 </div>
 </template>
+
+// fas fa-sync-alt fa-spin fa-5x
 
 <script>
 import GoalService from "../GoalsService";
@@ -45,7 +49,6 @@ export default {
 }
 
 .input-goal {
-    display: block;
     width: 100%;
     border: 1px solid #aecfa3;
     background-color: #6bbd98;
